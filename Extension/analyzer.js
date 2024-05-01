445f76053fb70950e3dfadbb7e7a8f08d5fea17c
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-02 01:31:28 +0530

    Added QnA Functionality.

--- Extension/analyzer.js
+++ Extension/analyzer.js
@@ -1,18 +1,42 @@
 const btn = document.getElementById("analyze");
-btn.addEventListener("click", function() {
-    btn.disabled = true;
-    btn.innerHTML = "Analyzing...";
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-        var url = tabs[0].url;
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
-        xhr.onload = function() {
-            var text = xhr.responseText;
-            const p = document.getElementById("output");
-            p.innerHTML = text;
-            btn.disabled = false;
-            btn.innerHTML = "Analyze";
+btn.addEventListener("click", function () {
+  btn.disabled = true;
+  btn.innerHTML = "Analyzing...";
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    var url = tabs[0].url;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+    xhr.onload = function () {
+      var text = xhr.responseText;
+      const p = document.getElementById("output");
+      p.innerHTML = text;
+      const btn2 = document.getElementById("search");
+      btn2.addEventListener("click", function () {
+        var inputValue = document.getElementById("follow_up").value;
+        if (inputValue.trim() === "") {
+          const p = document.getElementById("answer");
+          p.innerHTML = "Please put out your doubts in input box";
+        } else {
+          var zhr = new XMLHttpRequest();
+          zhr.open(
+            "GET",
+            "http://127.0.0.1:5000/answer?summary=" +
+              text +
+              " The text provided here is the transcript of the YouTube Video. Based on the text provided, please answer this question: " +
+              inputValue,
+            true
+          );
+          zhr.onload = function () {
+            var ans = zhr.responseText;
+            const x = document.getElementById("answer");
+            x.innerHTML = ans;
+          };
+          zhr.send();
         }
-        xhr.send();
-    });
+      });
+      btn.disabled = false;
+      btn.innerHTML = "Analyze";
+    };
+    xhr.send();
+  });
 });
--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, request
 from youtube_transcript_api import YouTubeTranscriptApi
 from transformers import pipeline
+import google.generativeai as genai
 
 app = Flask(__name__)
-
 
 @app.get('/summary')
 def summary_api():
@@ -28,6 +28,53 @@ def get_summary(transcript):
         summary = summary + summary_text + ' '
     return summary
 
+@app.get('/answer')
+def answer_api():
+    summary = request.args.get('summary','')
+    question = summary.split('=')[0]
+    answer = get_answer(question)
+    return answer
+
+def get_answer(question):
+
+    convo = question
+
+    genai.configure(api_key="AIzaSyCSLT7qkguZQWQ-i_TeUN6gCqRH_fRzqxw")
+
+    # Set up the model
+    generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+    }
+
+    safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+    },
+    ]
+
+    model = genai.GenerativeModel(model_name="gemini-1.0-pro", generation_config=generation_config, safety_settings=safety_settings)
+
+    conversation = model.start_chat(history=[])
+
+    conversation.send_message(convo)
+
+    return conversation.last.text
 
 if __name__ == '__main__':
     app.run()

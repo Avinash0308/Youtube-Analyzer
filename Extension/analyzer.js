1d370250721c93be7c72842cd27cdfa4f2f689d7
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-03 21:52:46 +0530

    Update.

--- Extension/analyzer.js
+++ Extension/analyzer.js
@@ -3,28 +3,37 @@ const btn = document.getElementById("analyze");
 btn.addEventListener("click", function () {
   btn.disabled = true;
   btn.innerHTML = "Analyzing... ";
-  document.getElementById("dot_loader").style.display="block";
+
+  document.getElementById("dot_loader").style.display = "block";
+
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     var url = tabs[0].url;
+
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://127.0.0.1:5000/summary?url=" + url, true);
+
     xhr.onload = function () {
       var text = xhr.responseText;
+
       const p = document.getElementById("output");
       p.innerHTML = text;
+
       document.getElementById("cont").style.display = "block";
       document.getElementById("output1").style.display = "block";
+
       const btn2 = document.getElementById("search");
+
       btn2.addEventListener("click", function () {
-        document.getElementById("goloader").style.display="block";
+        document.getElementById("goloader").style.display = "block";
+
         var inputValue = document.getElementById("follow_up").value;
-        
+
         if (inputValue.trim() === "") {
           const p = document.getElementById("answer");
           p.innerHTML = "Please put out your doubts in input box";
         } else {
-          
           var zhr = new XMLHttpRequest();
+
           zhr.open(
             "GET",
             "http://127.0.0.1:5000/answer?summary=" +
@@ -33,22 +42,24 @@ btn.addEventListener("click", function () {
               inputValue,
             true
           );
+
           zhr.onload = function () {
             var ans = zhr.responseText;
             const x = document.getElementById("answer");
             x.innerHTML = ans;
-            document.getElementById("goloader").style.display="none";
-
+            document.getElementById("goloader").style.display = "none";
           };
+
           zhr.send();
         }
       });
-  
-      document.getElementById("dot_loader").style.display="none";
-    
+
+      document.getElementById("dot_loader").style.display = "none";
+
       btn.disabled = false;
       btn.innerHTML = "Analyze";
     };
+
     xhr.send();
   });
 });

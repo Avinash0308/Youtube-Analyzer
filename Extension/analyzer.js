20395f23f48f9357c64e7eed258c8c4415b5b3a3
Avinash Agrawal <avinash03082003@gmail.com>
2024-05-29 00:27:20 +0530

    Enhanced Follow Up Questions Responses.

--- Extension/analyzer.js
+++ Extension/analyzer.js
@@ -34,26 +34,33 @@ btn.addEventListener("click", function () {
           document.getElementById("follow_up").style.backgroundImage =
             "url(Images/2.png)";
         } else {
-          var zhr = new XMLHttpRequest();
+          var transcript;
+          var yhr = new XMLHttpRequest();
+          yhr.open("GET", "http://127.0.0.1:5000/transcript?url=" + url, true);
+          yhr.onload = function () {
+            transcript = yhr.responseText;
 
-          zhr.open(
-            "GET",
-            "http://127.0.0.1:5000/answer?summary=" +
-              " Hey, I'll provide you transcript of the YouTube Video. Based on the transcript, please answer the question or perform the task as instructed in the query. Also, if you are unable to respond to the query using the text provided, try to use your general knowledge and common sense to provide best possible solution. Here is the text: " +
-              text +
-              ". And following is the query/task: " +
-              inputValue,
-            true
-          );
+            var zhr = new XMLHttpRequest();
+            zhr.open(
+              "GET",
+              "http://127.0.0.1:5000/answer?summary=" +
+                " Hey, I'll provide you transcript of the YouTube Video. Based on the transcript, please answer the question or perform the task as instructed in the query. Also, if you are unable to respond to the query using the text provided, try to use your general knowledge and common sense to provide best possible solution. Here is the transcript: " +
+                transcript +
+                ". And following is the query/task: " +
+                inputValue,
+              true
+            );
 
-          zhr.onload = function () {
-            var ans = zhr.responseText;
-            const x = document.getElementById("answer");
-            x.innerHTML = ans;
-            document.getElementById("follow_up").style.backgroundImage =
-              "url(Images/2.png)";
+            zhr.onload = function () {
+              var ans = zhr.responseText;
+              const x = document.getElementById("answer");
+              x.innerHTML = ans;
+              document.getElementById("follow_up").style.backgroundImage =
+                "url(Images/2.png)";
+            };
+            zhr.send();
           };
-          zhr.send();
+          yhr.send();
         }
         document.getElementById("follow_up").value = "";
       });
